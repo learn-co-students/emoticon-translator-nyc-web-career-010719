b2fadf8d4ab43984de9554acab61f379ac64fe09
github username <github email address>
2019-01-09 14:29:03 -0500

    test

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,33 @@
-# require modules here
+# require moduleres here
+require "pry"
+def load_library(file_path)
+  emoticon_dictionary = {}
+  emoticon_dictionary["get_meaning"] = {}
+  emoticon_dictionary["get_emoticon"] ={}
 
-def load_library
-  # code goes here
+  emoticon_file = YAML.load_file(file_path)
+  emoticon_file.each do |meaning, emoticons_arr|
+    emoticons_arr.each_with_index do |language, i|
+      usa = language[0]
+      japanese = language[1]
+
+      emoticon_dictionary["get_meaning"][japanese] = emoticon_text
+      emoticon_dictionary["get_emoticon"][usa] = japanese
+    end
+  end
+
+emoticon_dictionary
+
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  load_library(file_path)
+    if emoticon_dictionary["get_emoticon"][usa] == emoticon
+      binding.pry
+      return emoticon_dictionary["get_emoticon"]
+    else
+      return "Sorry, that emoticon was not found"
+    end
 end
 
 def get_english_meaning
